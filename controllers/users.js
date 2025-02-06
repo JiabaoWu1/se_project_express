456f3198adbc9bef116daa46e7bacff379a9d001
JiabaoWu <wujiabaotonny@gmail.com>
2025-02-06 12:43:04 -0500

    uldate controller to rad email and password fields from the request body

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,10 @@ const {
   CREATED,
   BAD_REQUEST,
   NOT_FOUND,
+  CONFLICT,
   SERVER_ERROR,
+  UNAUTHORIZED,
+  FORBIDDEN,
 } = require("../utils/errors");
 
 const getUsers = (req, res) => {
@@ -21,13 +24,30 @@ const getUsers = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
-  User.create({ name, avatar })
-    .then((user) => res.status(CREATED).send(user))
+  const { name, avatar, email, password } = req.body;
+  //hash the password
+  bycrypt
+    .hash(password, 10)
+    .then((hash) => User.create({ name, avatar, email, password: hash }))
+
+    .then((user) => {
+      const userWithoutPassword = {
+        _id: user._id,
+        name: user.name,
+        avatar: user.avatar,
+        email: user.email,
+      };
+      res.send(userWithoutPassword);
+    })
     .catch((err) => {
-      console.error(err);
+      console.error("User creation error", err);
+      if (err.code === 11000) {
+        return res.status(CONFLICT).send({ messaege: "Email already exists" });
+      }
       if (err.name === "ValidationError") {
-        return res.status(BAD_REQUEST).send({ message: err.message });
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: "Email already exists" });
       }
       return res
         .status(SERVER_ERROR)
@@ -53,4 +73,27 @@ const getUser = (req, res) => {
     });
 };
 
-module.exports = { getUsers, createUser, getUser };
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(BAD_REQUEST)
+      .send({ message: "Email and password required" });
+  }
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      return res.status(OK).send({ token });
+    })
+    .catch((error) => {
+      console.error(error);
+      console.log(error.name);
+      return res.status(UNAUTH_ERROR).send({ message: error.message });
+    });
+};
+
+module.exports = { getUsers, createUser, getUser, login };
